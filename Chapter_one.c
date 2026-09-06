e33cc44c14065255d0f868acdb2dd82029596d1a
TemiGraceO <graceolorunfemi96@gmail.com>
2026-09-06 02:45:14 +0100

    Added Chapter 1

---
+++ Chapter_one.c
@@ -0,0 +1,17 @@
+/* This program is written 
+    to print my name,
+    uses \" to print favourite quote,
+    uses escape sequence \\ to print the following directory structure: c:\cygwin\home\administrator,
+    and prints a diamond.
+ */
+
+#include <stdio.h>
+
+int main(){
+    printf("Grace Temitayo Olorunfemi.\n");
+    printf("This is my Favourite quote \"Happiness is Free\".\n");
+    printf("c:\\cygwin\\home\\administrator\n");
+    printf("\t\t\t*\n\t\t*\t\t*\n\t*\t\t\t\t*\n*\t\t\t\t\t\t*\t\t\n\t*\t\t\t\t*\n\t\t*\t\t*\n\t\t\t*\n");
+    printf("Sun\tMon\tTue\tWed\tThu\tFri\tSat\n\t\t 1\t 2\t 3\t 4\t 5\n 6\t 7\t 8\t 9\t 10\t 11\t 12\n 13\t 14\t 15\t 16\t 17\t 18\t 19\n 20\t 21\t 22\t 23\t 24\t 25\t 26\n 27\t 28\t 29\t 30\t ");
+    return 0;
+}
---
+++ first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    printf("C is working.\nI want to become an Embedded Systems Engineer\nI Love coding in C\n");
+    printf("Good ");
+    printf("Morning \n");
+    printf("Mon\t Tuesday\tThursday\n");
+    printf("1\t\t2\t\t\t3\n");
+    printf("Cursor to the beginning of the line\r");
+    printf("Trying backslash \\");
+    printf("Trying Forward Slash /\n");
+    printf("This is a double quote \"Think Big\"\n");
+    printf("This is an example of single quote \'Think Big\'\n"); 
+    //fflush(stdout) helps in debugging incase of errors
+    fflush(stdout);
+    return 0;
+}
